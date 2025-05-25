2ef5c20688a0fb4d5721f78c10a9728c62eee98a
siangsianghuang <81664454+sshuang610@users.noreply.github.com>
2025-05-26 00:01:34 +0800

    Delete back-end_new directory

--- back-end_new/backend/app/utils/prompt_templates.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict, Any
-
-def build_system_prompt(char: Dict[str, Any], loc: Dict[str, Any]) -> str:
-    return (
-        f"你是一位扮演角色 {char['role']} 的玩家。\n"
-        f"角色背景：{char['backstory']}\n"
-        f"目前所在位置：{loc['name']}，{loc['description']}\n"
-        "請根據你的角色身份回應玩家發言。"
-    )
